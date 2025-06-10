320f8ca4c3d5d05054710cbf488fc19522f8bee6
Layon Oliveira <layon.oliveira.lyn@gmail.com>
2025-06-10 10:41:32 -0300

    Testando a primeira classe de dominio

---
+++ src/main/java/com/devlyn/resources/CategoriesResource.java
@@ -0,0 +1,29 @@
+package com.devlyn.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devlyn.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categories")
+public class CategoriesResource {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+	}
+	
+}

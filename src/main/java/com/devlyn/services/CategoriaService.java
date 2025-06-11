670fe8e3aa9e1674e5ce92b6f3200c4bd46990f8
Layon Oliveira <layon.oliveira.lyn@gmail.com>
2025-06-10 22:24:35 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/devlyn/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.devlyn.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devlyn.domain.Categoria;
+import com.devlyn.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+	
+}

3e8c0a1db0c2e31af3e1ce0900ddbf2744fb43e0
Layon Oliveira <layon.oliveira.lyn@gmail.com>
2025-06-10 22:59:51 -0300

    Produtos e associacao muitos para muitos

---
+++ src/main/java/com/devlyn/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.devlyn.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devlyn.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository <Produto, Integer> {
+
+}

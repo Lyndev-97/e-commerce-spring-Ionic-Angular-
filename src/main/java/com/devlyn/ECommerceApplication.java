9403a7743644235e9f35f0bd2a710ffaca08bd4a
Layon Oliveira <layon.oliveira.lyn@gmail.com>
2025-06-24 20:05:18 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/devlyn/ECommerceApplication.java
+++ src/main/java/com/devlyn/ECommerceApplication.java
@@ -52,6 +52,15 @@ public class ECommerceApplication implements CommandLineRunner {
 		
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat3 = new Categoria(null, "Domesticos");
+		Categoria cat4 = new Categoria(null, "Eletronicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
+		Categoria cat8 = new Categoria(null, "Eventos");
+		Categoria cat9 = new Categoria(null, "Obras");
+		Categoria cat10 = new Categoria(null, "Mecanicas");
+		
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -64,7 +73,7 @@ public class ECommerceApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 

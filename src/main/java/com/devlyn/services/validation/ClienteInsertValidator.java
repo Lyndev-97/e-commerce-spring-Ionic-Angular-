d7ad01abe164f859081f6a5973d61c9d3b7dca0a
Layon Oliveira <layon.oliveira.lyn@gmail.com>
2025-06-30 10:26:11 -0300

    Validacao customizada de CPF e CNPJ na insercao de Cliente

---
+++ src/main/java/com/devlyn/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,39 @@
+package com.devlyn.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.devlyn.domain.enums.TipoCliente;
+import com.devlyn.dto.ClienteNewDTO;
+import com.devlyn.resources.exception.FieldMessage;
+import com.devlyn.services.validation.utils.BR;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+
+		List<FieldMessage> list = new ArrayList<>();
+
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}

e55b8c39c14f21937987a9994c7c49bb7d74a682
Layon Oliveira <layon.oliveira.lyn@gmail.com>
2025-06-30 10:36:53 -0300

    Validacao de email repetido em insercao de novo cliente

--- src/main/java/com/devlyn/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/devlyn/services/validation/ClienteInsertValidator.java
@@ -3,8 +3,12 @@ package com.devlyn.services.validation;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.devlyn.domain.Cliente;
 import com.devlyn.domain.enums.TipoCliente;
 import com.devlyn.dto.ClienteNewDTO;
+import com.devlyn.repositories.ClienteRepository;
 import com.devlyn.resources.exception.FieldMessage;
 import com.devlyn.services.validation.utils.BR;
 
@@ -12,6 +16,10 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +36,9 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) list.add(new FieldMessage("email", "Email já existente"));
 
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();

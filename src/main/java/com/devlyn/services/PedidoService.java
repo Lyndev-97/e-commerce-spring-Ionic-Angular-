b4f4233ae3e1e3fcb2414c064da279c14f221ae4
Layon Oliveira <layon.oliveira.lyn@gmail.com>
2025-07-10 09:05:07 -0300

    MockEmailService com Logger. Padroes strategy e Template Method

--- src/main/java/com/devlyn/services/PedidoService.java
+++ src/main/java/com/devlyn/services/PedidoService.java
@@ -31,6 +31,8 @@ public class PedidoService {
 	private ProdutoService produtoService;
 	@Autowired
 	private ClienteService clienteService;
+	@Autowired
+	private EmailService emailService;
 	
 	
 	public Pedido find(Integer id) {
@@ -60,7 +62,7 @@ public class PedidoService {
 			
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
-		System.out.println(obj);
+		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
 	}
 	

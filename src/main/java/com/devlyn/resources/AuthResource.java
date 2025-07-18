0805ab4b48827d8ff424d3c7517122f6cf2d4a45
Layon Oliveira <layon.oliveira.lyn@gmail.com>
2025-07-18 17:21:27 -0300

    refresh token

---
+++ src/main/java/com/devlyn/resources/AuthResource.java
@@ -0,0 +1,29 @@
+package com.devlyn.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devlyn.security.JWTUtil;
+import com.devlyn.security.UserSS;
+import com.devlyn.services.UserService;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+}
